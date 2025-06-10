aee1dd4f0a4a2e4e2b7f1dc1bb2df26eb1cb7f36
Tzu-Yao Lin <tzuyao.nick.lin@gmail.com>
2025-06-10 22:08:21 +0200

    Refine sbatch ntasks and array

--- job/run_parallel.sh
+++ job/run_parallel.sh
@@ -1,6 +1,4 @@
 #!/bin/bash -l
 
-module --force purge
-module use /apps/leuven/rocky8/icelake/2021a/modules/all
-module load worker/1.6.12-foss-2021a
+module load worker
 wsub -batch sim_3l-lmm_all.slurm -data sim_3l-lmm_all_pars.csv
