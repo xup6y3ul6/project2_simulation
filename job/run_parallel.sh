57c31dad894ff530d96dc6f80f513283b6511907
Tzu-Yao Lin <tzuyao.nick.lin@gmail.com>
2025-06-11 16:41:12 +0200

    submit all jobs

--- job/run_parallel.sh
+++ job/run_parallel.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -l
 
-module load worker
+module use /apps/leuven/rocky8/icelake/2021a/modules/all
+module load worker/1.6.12-foss-2021a-wice
 wsub -batch sim_3l-lmm_all.slurm -data sim_3l-lmm_all_pars.csv
